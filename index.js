681dff336bc609aec36fecb659e7af8c496a7870
dmadust@gmail.com <dmadust@gmail.com>
2018-04-21 06:28:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,4 +60,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   }
+  var total = total();
+  cart = [];
+  return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
 }
